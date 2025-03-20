21668553bb1637db620b5cb6e8e526bea9687d3c
sathya <sathyavgc@gmai.com>
2025-03-20 13:25:07 +0530

    init

---
+++ app/src/main/java/com/flow/mailflow/ChatActivity.java
@@ -0,0 +1,6 @@
+package com.flow.mailflow;
+
+import android.app.Activity;
+
+public class ChatActivity extends Activity {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("com.android.application") version "8.5.0" apply false // Keep at 8.5.0
+        id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MailFlow"
+include(":app")
